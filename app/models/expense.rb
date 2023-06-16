39ee3db8569add39069919ca12e8ff67993a57ef
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-06-16 23:27:01 +0000

    Fix rubocop offenses

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,6 +3,4 @@ class Expense < ApplicationRecord
   has_and_belongs_to_many :categories
 
   validates :name, :amount, presence: true
-
 end
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,3 @@ class User < ApplicationRecord
   has_many :categories
   has_many :expenses, foreign_key: :author_id
 end
-
-
