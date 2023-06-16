7de87df45c5cfe185946edcbbd17f1ed55c0d05d
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-06-16 23:24:06 +0000

    Add home and catgories controllers before switching branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'categories/index'
+  get 'home/index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # root to: 'home#index'
+
+
+  get '/home', to: 'home#index'
+  
+
+  
+  resources :categories do
+    resources :expenses
+  end
+  # resources :expenses
+  devise_for :users
+  
+
+  root 'home#index'
 end
